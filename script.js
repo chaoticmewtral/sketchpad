bb37250f4e0ac548918b219a3d41ad74c84b0e14
Remy <remy@chaoticmewtral.com>
2022-10-05 16:35:14 -0400

    Add function to insert divs in script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const container = document.querySelector('#container');
 
 const square = document.createElement('div');
 SVGTextContentElement.classList.add('square');
+
+let num = 16;
+
+// insert divs equal to the square of the number given
+function addGrid(num) {
+    for (let i = 0; i < (num ** 2); i++) {
+        container.appendChild(square);
+    }
+}
