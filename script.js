8933c1a10dffb878e160d4badec781c284ec4a43
Remy <remy@chaoticmewtral.com>
2022-10-05 18:34:42 -0400

    Add to script.js and style.css

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const container = document.querySelector('#container');
 const square = document.createElement('div');
 let num = 16;
 
-for (let i=0; i < (num ** 2); i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-}
+const squareSize = (960/num);
+
+    for (let i=0; i < (num ** 2); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.height = `squareSize`;
+        square.style.width = `squareSize`;
+        container.appendChild(square);
+    }
+
+
+
+// assign size attribute based on num
