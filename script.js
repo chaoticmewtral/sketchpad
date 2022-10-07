8f3b9ac14f08c6ce55aa8235b18d7c9518c3a2a1
Remy <remy@chaoticmewtral.com>
2022-10-07 13:39:31 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const squareSize = (960/num);
     for (let i=0; i < (num ** 2); i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.style.height = `squareSize`;
-        square.style.width = `squareSize`;
+        square.style.height = squareSize + 'px';
+        square.style.width = squareSize + 'px';
         container.appendChild(square);
     }
 
