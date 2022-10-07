a1015278400c3671af2b478b3ab6d11ebe0a71e2
Remy <remy@chaoticmewtral.com>
2022-10-07 14:59:37 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('#container');
-const square = document.createElement('div');
+
 let num = 16;
 
 const squareSize = (960/num);
 
+function addGrid() {
     for (let i=0; i < (num ** 2); i++) {
         const square = document.createElement('div');
         square.classList.add('square');
@@ -11,4 +12,20 @@ const squareSize = (960/num);
         square.style.width = squareSize + 'px';
         container.appendChild(square);
     }
+}
 
+function fillPixel() {
+    const pixels = document.querySelectorAll('.square');
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseenter', (e) => {
+            pixel.classList.add('fillPixel');
+        });
+    });
+}
+
+function play() {
+    addGrid();
+    fillPixel();
+}
+
+window.addEventListener('load', play());
